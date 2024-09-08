05cc1893bcae200e631f3b707f05798d2cc46d50
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-09-08 17:36:32 -0400

    Draft quest map

--- demo_app/index.js
+++ demo_app/index.js
@@ -6,10 +6,10 @@ import { parkingStyleOptions as pso } from "./assets/js/parking_style_options.js
 const PARKING_LAYER = "byQuality";
 
 const map = new maplibregl.Map({
-  container: 'map',
-  style: 'https://api.maptiler.com/maps/streets/style.json?key=MlRGiaPF42qIx2cAP9Hn',
+  container: "map",
+  style: "https://api.maptiler.com/maps/streets/style.json?key=MlRGiaPF42qIx2cAP9Hn",
   center: [-79.416, 43.714],
-  zoom: 10.5
+  zoom: 10.5,
 });
 
 // set up bounding box of Toronto used to narrow down geolocation
@@ -28,103 +28,98 @@ const torontoBBox = new BBox(-79.6392832, -79.1132193, 43.5796082, 43.8554425);
 
 const geocoderApi = {
   forwardGeocode: async (config) => {
-      const features = [];
-      try {
-          const request =
-      `https://nominatim.openstreetmap.org/search?q=${
-          config.query
+    const features = [];
+    try {
+      const request = `https://nominatim.openstreetmap.org/search?q=${
+        config.query
       }&format=geojson&polygon_geojson=1&addressdetails=1&countrycodes=CA&viewbox=${torontoBBox.getURLParams()}&bounded=1`;
-          const response = await fetch(request);
-          const geojson = await response.json();
-          for (const feature of geojson.features) {
-              const center = [
-                  feature.bbox[0] +
-              (feature.bbox[2] - feature.bbox[0]) / 2,
-                  feature.bbox[1] +
-              (feature.bbox[3] - feature.bbox[1]) / 2
-              ];
-              const point = {
-                  type: 'Feature',
-                  geometry: {
-                      type: 'Point',
-                      coordinates: center
-                  },
-                  place_name: feature.properties.display_name,
-                  properties: feature.properties,
-                  text: feature.properties.display_name,
-                  place_type: ['place'],
-                  center
-              };
-              features.push(point);
-          }
-      } catch (e) {
-          console.error(`Failed to forwardGeocode with error: ${e}`);
+      const response = await fetch(request);
+      const geojson = await response.json();
+      for (const feature of geojson.features) {
+        const center = [
+          feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+          feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
+        ];
+        const point = {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: center,
+          },
+          place_name: feature.properties.display_name,
+          properties: feature.properties,
+          text: feature.properties.display_name,
+          place_type: ["place"],
+          center,
+        };
+        features.push(point);
       }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`);
+    }
 
-      return {
-          features
-      };
-  }
+    return {
+      features,
+    };
+  },
 };
 
-map.on('load', () => {
-  map.addControl(new maplibregl.NavigationControl(), 'top-left');
-  map.addControl(new maplibregl.GeolocateControl(), 'top-left');
+map.on("load", () => {
+  map.addControl(new maplibregl.NavigationControl(), "top-left");
+  map.addControl(new maplibregl.GeolocateControl(), "top-left");
   map.addControl(
     new MaplibreGeocoder(geocoderApi, {
-        maplibregl
+      maplibregl,
     })
   );
 
   // add in data sources
-  const displayData = "https://raw.githubusercontent.com/tallcoleman/new-parking-map/main/Display%20Files/all_sources.geojson";
+  const displayData =
+    "https://raw.githubusercontent.com/tallcoleman/new-parking-map/main/Display%20Files/all_sources.geojson";
   const bikeLaneURL = "data/cycling-network.geojson";
   const bikeTheftURL = "data/bicycle-thefts.geojson";
 
-  map.addSource('bicycle-parking', {
-    'type': 'geojson',
-    'data': displayData,
-    'generateId': true,
+  map.addSource("bicycle-parking", {
+    type: "geojson",
+    data: displayData,
+    generateId: true,
   });
-  map.addSource('bicycle-lanes', {
-    'type': 'geojson',
-    'data': bikeLaneURL,
+  map.addSource("bicycle-lanes", {
+    type: "geojson",
+    data: bikeLaneURL,
   });
-  map.addSource('bicycle-thefts', {
-    'type': 'geojson',
-    'data': bikeTheftURL,
-  });  
+  map.addSource("bicycle-thefts", {
+    type: "geojson",
+    data: bikeTheftURL,
+  });
 
   // Add a layer showing bike lanes
   // line-dasharray doesn't support data-driven styling, so the dashed and non-dashed lines have to be added as separate layers
   map.addLayer({
-    'id': 'bicycle-lanes',
-    'type': 'line',
-    'source': 'bicycle-lanes',
-    'filter': [
+    id: "bicycle-lanes",
+    type: "line",
+    source: "bicycle-lanes",
+    filter: [
       "match",
       ["get", "INFRA_HIGHORDER"],
       [
         "Sharrows - Wayfinding",
         "Sharrows - Arterial - Connector",
         "Signed Route (No Pavement Markings)",
-        "Sharrows"
-      ], false, 
+        "Sharrows",
+      ],
+      false,
       true,
     ],
-    'layout': {
-      'line-cap': 'round',
+    layout: {
+      "line-cap": "round",
     },
-    'paint': {
-      'line-width': 3,
-      'line-color': [
+    paint: {
+      "line-width": 3,
+      "line-color": [
         "match",
         ["get", "INFRA_HIGHORDER"],
-        [
-          "Cycle Track",
-          "Cycle Track - Contraflow",
-          "Bi-Directional Cycle Track"
-        ],
+        ["Cycle Track", "Cycle Track - Contraflow", "Bi-Directional Cycle Track"],
         "hsl(137, 68%, 23%)",
         [
           "Multi-Use Trail - Boulevard",
@@ -132,140 +127,146 @@ map.on('load', () => {
           "Multi-Use Trail - Existing Connector",
           "Multi-Use Trail - Connector",
           "Multi-Use Trail",
-          "Park Road"
+          "Park Road",
         ],
         "#8c5535",
-        [
-          "Bike Lane - Buffered",
-          "Bike Lane",
-          "Bike Lane - Contraflow"
-        ],
+        ["Bike Lane - Buffered", "Bike Lane", "Bike Lane - Contraflow"],
         "hsl(137, 68%, 36%)",
-        "#2c3b42"
-      ]
+        "#2c3b42",
+      ],
     },
-  })
+  });
   map.addLayer({
-    'id': 'bicycle-routes',
-    'type': 'line',
-    'source': 'bicycle-lanes',
-    'filter': [
+    id: "bicycle-routes",
+    type: "line",
+    source: "bicycle-lanes",
+    filter: [
       "match",
       ["get", "INFRA_HIGHORDER"],
       [
         "Sharrows - Wayfinding",
         "Sharrows - Arterial - Connector",
         "Signed Route (No Pavement Markings)",
-        "Sharrows"
-      ], true, 
+        "Sharrows",
+      ],
+      true,
       false,
     ],
-    'layout': {
-      'line-cap': 'round',
+    layout: {
+      "line-cap": "round",
     },
-    'paint': {
-      'line-width': 3,
-      'line-dasharray': [1, 2],
-      'line-color': [
+    paint: {
+      "line-width": 3,
+      "line-dasharray": [1, 2],
+      "line-color": [
         "match",
         ["get", "INFRA_HIGHORDER"],
         [
           "Sharrows - Wayfinding",
           "Sharrows - Arterial - Connector",
           "Signed Route (No Pavement Markings)",
-          "Sharrows"
+          "Sharrows",
         ],
         "hsl(137, 56%, 62%)",
-        "#2c3b42"
-      ]
+        "#2c3b42",
+      ],
     },
-  })  
+  });
 
   // Add a layer showing the parking
   map.addLayer({
-      'id': 'bicycle-parking-nodes',
-      'type': pso[PARKING_LAYER].nodes.type,
-      'source': 'bicycle-parking',
-      'filter': [
-        "match",
-        ['geometry-type'], 
-        ['Point'], true,
-        false,
+    id: "bicycle-parking-nodes",
+    type: pso[PARKING_LAYER].nodes.type,
+    source: "bicycle-parking",
+    // TODO has "source" attribute (ciy only), is rack...
+    filter: [
+      "all",
+      ["match", ["geometry-type"], ["Point"], true, false],
+      ["in", "open.toronto.ca", ["get", "meta_source"]],
+      [
+        "any",
+        [
+          "match",
+          ["get", "meta_source"],
+          "https://open.toronto.ca/dataset/street-furniture-bicycle-parking/",
+          false,
+          true,
+        ],
+        ["!", ["has", "bicycle_parking"]],
+        ["match", ["get", "bicycle_parking"], "rack", true, false],
       ],
-      'paint': pso[PARKING_LAYER].nodes.paint,
+    ],
+    paint: pso[PARKING_LAYER].nodes.paint,
   });
   map.addLayer({
-    'id': 'bicycle-parking-ways',
-    'type': pso[PARKING_LAYER].ways.type,
-    'source': 'bicycle-parking',
-    'filter': [
-      "match",
-      ['geometry-type'], 
-      ['LineString'], true,
-      false,
-    ],
-    'paint': pso[PARKING_LAYER].ways.paint,
+    id: "bicycle-parking-ways",
+    type: pso[PARKING_LAYER].ways.type,
+    source: "bicycle-parking",
+    filter: ["match", ["geometry-type"], ["LineString"], true, false],
+    paint: pso[PARKING_LAYER].ways.paint,
   });
 
-
   const ParkingPopUp = new PopUpHandler(
-    map, 
-    ['bicycle-parking-nodes', 'bicycle-parking-ways'],
+    map,
+    ["bicycle-parking-nodes", "bicycle-parking-ways"],
     document.getElementById("feature-details"),
     document.body,
     "layout-popup"
-  )
+  );
 
-  map.on('click', (e) => {ParkingPopUp.fromPoint(e.point)});
+  map.on("click", (e) => {
+    ParkingPopUp.fromPoint(e.point);
+  });
 
   document.getElementById("button-biketheft").addEventListener("click", () => {
     // Add bike theft heatmap
-    map.addLayer({
-      'id': 'bicycle-thefts',
-      'type': 'heatmap',
-      'source': 'bicycle-thefts',
-      'filter': [
-        'match',
-        ['get', 'PREMISES_TYPE'],
-        [
-          "Outside",
-          "Transit",
-        ], true, 
-        false,
-      ],
-      'paint': {
-        'heatmap-radius': {
-          'base': 2,
-          'stops': [
-            [
-              10,
-              2
-            ],
-            [
-              19,
-              1028
-            ]
-          ]
-        },
-        'heatmap-color': [
-          "interpolate",
-          ["linear"],
-          ["heatmap-density"],
-          0,"rgba(0, 0, 255, 0)",
-          0.3,"royalblue",
-          0.6,"cyan",
-          0.9,"lime",
-          0.999,"yellow",
-          1,"red"
+    map.addLayer(
+      {
+        id: "bicycle-thefts",
+        type: "heatmap",
+        source: "bicycle-thefts",
+        filter: [
+          "match",
+          ["get", "PREMISES_TYPE"],
+          ["Outside", "Transit"],
+          true,
+          false,
         ],
-        'heatmap-opacity': 0.5,
-      }
-    }, "bicycle-lanes");
+        paint: {
+          "heatmap-radius": {
+            base: 2,
+            stops: [
+              [10, 2],
+              [19, 1028],
+            ],
+          },
+          "heatmap-color": [
+            "interpolate",
+            ["linear"],
+            ["heatmap-density"],
+            0,
+            "rgba(0, 0, 255, 0)",
+            0.3,
+            "royalblue",
+            0.6,
+            "cyan",
+            0.9,
+            "lime",
+            0.999,
+            "yellow",
+            1,
+            "red",
+          ],
+          "heatmap-opacity": 0.5,
+        },
+      },
+      "bicycle-lanes"
+    );
   });
-
 });
 
 // zoom indicator for debug
-map.on('zoomend', () => {
-  document.getElementById('zoom-indicator').textContent = "Zoom: " + Math.round(map.getZoom() * 10) / 10;
-})
+map.on("zoomend", () => {
+  document.getElementById("zoom-indicator").textContent =
+    "Zoom: " + Math.round(map.getZoom() * 10) / 10;
+});
