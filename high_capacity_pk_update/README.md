5aab57efb03839a468f4527922469c92c9dab6ac
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-09-22 14:25:04 -0400

    Created and ran primary key update script
    for bicycle-parking-high-capacity-outdoor dataset

---
+++ high_capacity_pk_update/README.md
@@ -0,0 +1,25 @@
+# Primary Key update script for bicycle-parking-high-capacity-outdoor dataset
+
+## Purpose
+
+The `ID` primary key field in the [bicycle-parking-high-capacity-outdoor](https://open.toronto.ca/dataset/bicycle-parking-high-capacity-outdoor/) City of Toronto dataset has unfortunately not remained stable over time. The purpose of this script is to help update uses of this field in OpenStreetMap data tagged with `ref:open.toronto.ca:bicycle-parking-high-capacity-outdoor:id`.
+
+## How to run
+
+```bash
+$ . .venv/bin/activate
+$ python high_capacity_pk_update/high_capacity_pk_update.py
+```
+
+The script will generate a folder in `high_capacity_pk_update/output`. Each sub-folder will show information about subsequent pairs from dated folders in the `Output Files` directory:
+
+- Spatial duplicates encountered in the source data for `bicycle-parking-high-capacity-outdoor` (for both files in the pair)
+- Any files that were not able to be matched by sharing the same geometry (e.g. if new points are added from one week to another, or geometry changes are made in the source data)
+- Any changes to the primary key `ID` field from one week to another
+
+## Intended usage
+
+- Generate the output files
+- If there is a folder with an "id_change" file, use that file in JOSM along with the Overpass Query below and the conflation plugin to update the values for `ref:open.toronto.ca:bicycle-parking-high-capacity-outdoor:id`
+- Check for cases of mapped values that are not spatially unique in the `bicycle-parking-high-capacity-outdoor` dataset
+- Check for cases of values in the `bicycle-parking-high-capacity-outdoor` that were not matched from one week to another
---
+++ high_capacity_pk_update/high_capacity_pk_update.py
@@ -0,0 +1,64 @@
+# Script to update primary keys for bicycle-parking-high-capacity dataset used in OpenStreetMap
+
+from pathlib import Path
+import os
+
+import geopandas as gpd
+
+ARCHIVE_PATH = Path("Source Files")
+DATASET_NAME = "bicycle-parking-high-capacity-outdoor"
+OUTPUT_PATH = Path("high_capacity_pk_update") / "output"
+
+
+def get_spatial_duplicates(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Returns only rows that are spatial duplicates"""
+    return gdf[gdf["geometry"].duplicated(False)]
+
+
+def agg_spatial_duplicates(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
+    """Aggregates points that are spatial duplicates (e.g. two points with the same geometry, one has ID "23" and the other ID "46", returns only one row with the ID "23; 46")"""
+    aggregated = (
+        gdf
+        .groupby("geometry", as_index=False)
+        .aggregate(lambda x: ";".join([str(_) for _ in x]))
+    )
+    return gpd.GeoDataFrame(aggregated, geometry=aggregated["geometry"])
+
+
+def find_pk_change():
+    """Searches subsequent pairs of folders in /Output Files and outputs informational files for the bicycle-parking-high-capacity-outdoor dataset indicating the success of matching them spatially from one week to the next and whether the ID primary key has changed."""
+    archive_folders = sorted([f for f in ARCHIVE_PATH.iterdir() if f.is_dir()])
+    folder_pairs = list(zip(archive_folders[:-1], archive_folders[1:]))
+
+    for first, second in folder_pairs:
+        first_file = first / (DATASET_NAME + ".geojson")
+        second_file = second / (DATASET_NAME + ".geojson")
+        first_gdf = gpd.read_file(first_file)
+        second_gdf = gpd.read_file(second_file)
+        first_gdf_sdupe = get_spatial_duplicates(first_gdf)
+        second_gdf_sdupe = get_spatial_duplicates(second_gdf)
+        first_agg = agg_spatial_duplicates(first_gdf[["geometry", "ID"]])
+        second_agg = agg_spatial_duplicates(second_gdf[["geometry", "ID"]])
+        sjoined = first_agg.sjoin(second_agg, how="left")
+        no_match = sjoined[sjoined["ID_right"].isna()]
+        id_change = sjoined[sjoined["ID_left"] != sjoined["ID_right"]]
+
+        # output
+        output_dir = OUTPUT_PATH / f"{first.name}_{second.name}"
+        os.makedirs(output_dir, exist_ok=True)
+
+        first_gdf_sdupe.to_file(
+            output_dir / "first_spatial_dupes.geojson", driver="GeoJSON")
+        second_gdf_sdupe.to_file(
+            output_dir / "second_spatial_dupes.geojson", driver="GeoJSON")
+
+        if len(no_match) > 0:
+            no_match.to_file(output_dir / "no_match.geojson", driver="GeoJSON")
+
+        if len(id_change) > 0:
+            id_change.to_file(
+                output_dir / "id_change.geojson", driver="GeoJSON")
+
+
+if __name__ == "__main__":
+    find_pk_change()
